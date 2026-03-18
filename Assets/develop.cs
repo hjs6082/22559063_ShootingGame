9a9f07c6bef63c65d76080b8c9a6e63df4104da7
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2026-03-18 13:48:59 +0900

    feat: 수정

--- Assets/develop.cs
+++ Assets/develop.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class develop : MonoBehaviour
+public class Develop : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Develop Branch에서 작성했습니다")
+        Debug.Log("Develop Branch에서 작성했습니다");
     }
 
     // Update is called once per frame
